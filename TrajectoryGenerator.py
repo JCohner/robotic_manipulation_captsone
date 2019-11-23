67e91ade006ec8925582324f7380a017595f4874
JCohner <joshuacohen2019@u.northwestern.edu>
2019-11-23 11:27:57 -0600

    added third waypoint out of 4, cleaned up code with function to pack csv

--- TrajectoryGenerator.py
+++ TrajectoryGenerator.py
@@ -2,6 +2,19 @@ import modern_robotics as mr
 import numpy as np
 import pandas as pd
 
+def to_csv(list,k, gtime, gstate, mode):
+	traj = np.array(list).reshape(k,-1)
+	df = pd.DataFrame(np.zeros((k,13)))
+	df.iloc[:,:3] = traj[:,:3]
+	df.iloc[:,3:6] = traj[:,4:7]
+	df.iloc[:,6:9] = traj[:,8:11]
+	df.iloc[:,9] = traj[:,3]
+	df.iloc[:,10] = traj[:,7]
+	df.iloc[:,11] = traj[:,11]
+	df.iloc[gtime:, 12] = gstate
+
+	df.to_csv("eggs.csv", header=False, index=False, mode = mode)
+
 def TrajectoryGenerator(Tse_init, Tsc_init, Tsc_final, Tce_grasp, Tce_standoff, k):
 	method = 5 #5th order poly
 	Tf = 5
@@ -11,36 +24,20 @@ def TrajectoryGenerator(Tse_init, Tsc_init, Tsc_final, Tce_grasp, Tce_standoff, 
 	Xend = np.matmul(Tsc_init, Tce_standoff)
 	#this is the call to position our gripper over the piece 
 	traj_s1 = mr.ScrewTrajectory(Xstart, Xend, Tf, N, method) 
-	#shape it to a 2d array of (100, 16)
-	traj_s1 = np.array(traj_s1).reshape(k,-1)
-	
-	df = pd.DataFrame(np.zeros((k,13)))
-	#pack data frame to csv specifications 
-	df.iloc[:,:3] = traj_s1[:,:3]
-	df.iloc[:,3:6] = traj_s1[:,4:7]
-	df.iloc[:,6:9] = traj_s1[:,8:11]
-	df.iloc[:,9] = traj_s1[:,3]
-	df.iloc[:,10] = traj_s1[:,7]
-	df.iloc[:,11] = traj_s1[:,11]
-	df.to_csv("eggs.csv", header=False, index=False)
+	to_csv(traj_s1, k, 0, 0, 'w')
 
-	df = pd.DataFrame(np.zeros((k,13)))
 	Xstart = Xend
 	Xend = np.matmul(Tsc_init, Tce_grasp)
 	Tf = 3
 	#this is the call to position our gripper over the piece 
 	traj_s2 = mr.ScrewTrajectory(Xstart, Xend, Tf, N, method) 
-	#shape it to a 2d array of (100, 16)
-	traj_s2 = np.array(traj_s2).reshape(k,-1)
-	print(traj_s2.shape)
-	df.iloc[:,:3] = traj_s2[:,:3]
-	df.iloc[:,3:6] = traj_s2[:,4:7]
-	df.iloc[:,6:9] = traj_s2[:,8:11]
-	df.iloc[:,9] = traj_s2[:,3]
-	df.iloc[:,10] = traj_s2[:,7]
-	df.iloc[:,11] = traj_s2[:,11]
-	df.iloc[50:,12] = 1
-	df.to_csv("eggs.csv", header=False, index=False, mode='a')
+	to_csv(traj_s2, k, 50, 1, 'a')
+
+	Xstart = Xend
+	Xend = np.matmul(Tsc_final,Tce_grasp)
+	Tf = 5
+	traj_s3 = mr.ScrewTrajectory(Xstart, Xend, Tf, N, method)
+	to_csv(traj_s3, k, 0, 1, 'a')
 
 if __name__ == '__main__':
 	Tse_init = np.array([[1, 0, 0, 0],
@@ -54,7 +51,7 @@ if __name__ == '__main__':
 				   	 	 [0, 0, 0, 1,]])
 
 	Tsc_final = np.array([[0, 1, 0, 0],
-				   	 	  [-1, 0, 0, 1],
+				   	 	  [-1, 0, 0, -1],
 				   	 	  [0, 0, 1, 0.025],
 				   	 	  [0, 0, 0, 1,]])
 
