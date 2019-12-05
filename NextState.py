8c012e27129bd14cc8a455a928132c9f3335784d
JCohner <joshuacohen2019@u.northwestern.edu>
2019-12-05 17:13:39 -0600

    got a working next state function

--- NextState.py
+++ NextState.py
@@ -2,22 +2,24 @@ import modern_robotics as mr
 import numpy as np
 import pandas as pd
 
-def to_csv(list,k, gtime, gstate, mode):
-	traj = np.array(list).reshape(k,-1)
-	df = pd.DataFrame(np.zeros((k,13)))
-	df.iloc[:,:3] = traj[:,:3]
-	df.iloc[:,3:6] = traj[:,4:7]
-	df.iloc[:,6:9] = traj[:,8:11]
-	df.iloc[:,9] = traj[:,3]
-	df.iloc[:,10] = traj[:,7]
-	df.iloc[:,11] = traj[:,11]
-	df.iloc[gtime:, 12] = gstate
-
-	df.to_csv("eggs.csv", header=False, index=False, mode = mode)
+def to_csv(p_list):
+	df = pd.DataFrame(p_list)
+	df.to_csv("eggs.csv", header=False, index=False)
 
 #q - config 12 x 1, thetadot, thetadot - control vector 9 x 1, dt - timestep, omega_max - max angular velocity of arm joints and wheels 
 def NextState(q, thetadot, dt, omega_max):
-	q_new = 1
+	#arm angles
+	for i in range(thetadot.shape[0]):
+		x = abs(thetadot[i])
+		if x > omega_max:
+			thetadot[i] = thetadot[i]/float(x) * omega_max
+	q_new = np.zeros(13)
+	q_new[3:8] = q[3:8] + thetadot[0:5] * dt
+	q_new[8:12] = q[8:12] + thetadot[5:9] * dt
+	l = 0.47/2 
+	w = .3/2
+	H_mat = np.array([[-1/(l+w), 1/(l+w), 1/(l+w), -1/(l+w)],[1,1,1,1],[-1,1,-1,1]])
+	q_new[0:3] = 0.0475/4. * np.matmul(H_mat, thetadot[5:9]) * dt + q[0:3]
 
 	#return the new configuration
 	return q_new
@@ -26,7 +28,16 @@ def main():
 	dt = 0.01
 	t_total = 1
 	tvec = np.arange(0,t_total,dt)
-	print(tvec.shape)
+	its = tvec.shape[0]
 
+	omega_max = 5
+	q_list = np.zeros((its,13))
+	q0 = np.array([0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) #x_chassis, y_chassis, phi_chassis, arm_theta1, arm_theta2, arm_theta3, arm_theta4, arm_theta5, wheel_theta1, wheel_theta2, wheel_theta3, wheel_theta4 
+	q_list[0] = q0
+	u_list = np.array([0,0,0,0,0, 10,10,10,10]) #arm_theta1, arm_theta2, arm_theta3, arm_theta4, arm_theta4, wheel_theta1, wheel_theta2, wheel_theta3, wheel_theta4
+	for i in range(1, its):
+		q_list[i] = NextState(q_list[i-1], u_list, dt, omega_max)
+	# NextState(q_list[0], u_list, dt, omega_max)
+	to_csv(q_list)
 if __name__ == '__main__':
 	main()
