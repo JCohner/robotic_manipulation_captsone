578b35567fade43548251f6c61e17059d00b395f
JCohner <joshuacohen2019@u.northwestern.edu>
2019-12-08 22:22:12 -0600

    commented manipulator code

--- code/MobileManipulator.py
+++ code/MobileManipulator.py
@@ -1,18 +1,22 @@
+#incldues from my own code
 from TrajectoryGenerator import TrajectoryGenerator, to_csv
 from NextState import NextState
 from FeedbackControl import FeedbackControl, Jacobian_in_Body_Pinv
 
+#standard python includes
 import numpy as np
 import pandas as pd
 import modern_robotics as mr
-
 import matplotlib.pyplot as plt
+import sys
+
 def main():
+	#initial configuration
 	q0 = np.array([np.pi/6.,.2,-.2,
 				   0,0,0.2,0,0,
 				   0,0,0,0,
 				   0])
-
+	#Here are configurations to calcualte Tse from an initial configuration, the requested end implementation does not take advantage of this, but was useful in debugging
 	B_list_arm = np.array([[0,0,0,0,0],
 						  [0,-1,-1,-1,0],
 						  [1,0,0,0,1],
@@ -35,14 +39,15 @@ def main():
 					[0,0,1,0.5],
 					[0,0,0,1]])
 	T0e = mr.FKinBody(M0e, B_list_arm, q0[3:8])
+	# Tse_init = np.matmul(np.matmul(Tsb,Tb0),T0e)
 
+	#the requested initial end effector config for submission
 	Tse_init = np.array([[0, 0, 1, 0.0],
 					     [0, 1, 0, 0],
 					     [-1, 0, 0, 0.653],
 					     [0, 0, 0, 1,]])
-	# Tse_init = np.matmul(np.matmul(Tsb,Tb0),T0e)
 
-
+	#initial and final positions of the cube along with standoff and grasping position of end effector relative to cube
 	Tsc_init = np.array([[1, 0, 0, 1],
 				   	 	 [0, 1, 0, 0],
 				   	 	 [0, 0, 1, 0.025],
@@ -63,43 +68,55 @@ def main():
 						  [-1/np.sqrt(2), 0, -1/np.sqrt(2), .05],
 						  [0, 0, 0, 1]])
 
-
+	#Definitons of constant and simulation parameterizing terms
 	k = 200
 	dt = 0.01
-	omega_max = 5
 
+	#Terms for feeback control to calculate velocity
+	omega_max = 5
 	Kp = np.identity(4) * 3 #np.zeros((4,4))
 	Ki = np.identity(4) * .1
 
+	#generation of trajectory
 	Te_traj, traj_df = TrajectoryGenerator(Tse_init, Tsc_init, Tsc_final, Tce_grasp, Tce_standoff, k)
 
-	print(traj_df.shape)
-
+	#intiialize variables that will make outputting to csv easier
 	config = pd.DataFrame(np.zeros((7*k,13)))
 	xerr = np.zeros((6,7*k))
 	config.iloc[0,:] = q0
 
+	#simulation loop
 	for i in range(1,6*k-1):
 		q = config.iloc[i-1,:].to_numpy()
 		
 		#get twist to next pose using feedback control
 		Je_pinv, Tbe = Jacobian_in_Body_Pinv(q[:8])
+		#get desired position
 		Xd = Te_traj[i,:,:]
-		Xd_next = Te_traj[i+1,:,:] 
+		#get next desired position
+		Xd_next = Te_traj[i+1,:,:]
+		#get current position 
 		Tsb = np.array([[np.cos(q[0]),-np.sin(q[0]),0,q[1]],
 					[np.sin(q[0]),np.cos(q[0]),0,q[2]],
 					[0,0,1,0.0963],
 					[0,0,0,1]])
 		X = np.matmul(Tsb,Tbe)
+		#get desired twist of end effector and corresponding calculated error
 		Ve, Xerr = FeedbackControl(X,Xd,Xd_next,Kp,Ki,dt)
 		xerr[:,-i] = Xerr
-		vels = np.matmul(Je_pinv, Ve) #TODO: make sure this is returning velocities in the same way NextState expects them
+		#calculate corresponding velocities
+		vels = np.matmul(Je_pinv, Ve) 		
 		#put vels in to get next state
 		config.iloc[i,:] = NextState(q, vels, dt, omega_max) 
 		if traj_df.iloc[i,12] == 1:
 			config.iloc[i,12] = 1
 
-
-	config.to_csv("chungus.csv", header=False, index=False)
+	if (sys.argv[1]):
+		name = sys.argv[1]
+	else:
+		name = 'eggs'
+		
+	#output to csv
+	config.to_csv("{}.csv".format(name), header=False, index=False)
 if __name__ == '__main__':
 	main()
